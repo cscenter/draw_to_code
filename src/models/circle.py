e880338d8a1baa4fadab3a4aed247d1abd478792
students <voivakin@beleriand.yandex.ru>
2018-12-09 06:05:34 -0500

    sync between pc

--- src/models/circle.py
+++ src/models/circle.py
@@ -36,15 +36,7 @@ def get_circle_model(image_size):
 def find_circle_model(image_size):
     model = keras.models.Sequential()
     model.add(L.InputLayer(input_shape=[image_size, image_size, 1]))
-    model.add(L.Conv2D(filters=40, kernel_size=(3, 3)))
-    model.add(L.BatchNormalization())
-    model.add(L.Activation('relu'))
-    model.add(L.Conv2D(filters=40, kernel_size=(3, 3)))
-    model.add(L.MaxPooling2D(pool_size=(2, 2)))
-    model.add(L.Activation('relu'))
-    model.add(L.Dropout(0.15))
-
-    model.add(L.Conv2D(filters=80, kernel_size=(3, 3)))
+    model.add(L.Conv2D(filters=200, kernel_size=(3, 3), strides=3))
     model.add(L.BatchNormalization())
     model.add(L.Activation('relu'))
     model.add(L.Conv2D(filters=80, kernel_size=(3, 3)))
@@ -52,16 +44,20 @@ def find_circle_model(image_size):
     model.add(L.Activation('relu'))
     model.add(L.Dropout(0.15))
 
-    model.add(L.Conv2D(filters=160, kernel_size=(3, 3)))
-    model.add(L.BatchNormalization())
-    model.add(L.Activation('relu'))
+    model.add(L.Conv2D(filters=80, kernel_size=(4, 4)))
     model.add(L.MaxPooling2D(pool_size=(2, 2)))
     model.add(L.Activation('relu'))
     model.add(L.Dropout(0.15))
+
+    model.add(L.Conv2D(filters=20, kernel_size=(5, 5)))
+    model.add(L.BatchNormalization())
+    model.add(L.Activation('relu'))
 
     model.add(L.Flatten())
-    model.add(L.Dense(700, activation='relu'))
-    model.add(L.Dropout(0.2))
+    model.add(L.Dense(600, activation='relu'))
+    model.add(L.Dropout(0.15))
+    model.add(L.Dense(200, activation='relu'))
+    model.add(L.Dropout(0.15))
     model.add(L.Dense(1, activation='softmax'))
 
     model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
--- src/nn_playground.py
+++ src/nn_playground.py
@@ -14,27 +14,38 @@ from models.segment import find_segment_model, get_segment_model
 def generate_data(pics_amount, image_size, figure_class, circles_amount, segments_amount):
     x_list = []
     y_list = []
+    # y_is_fig_here_list = []
 
     for _ in range(pics_amount):
         im, figures = generate_random_pic(image_size, circles_amount, segments_amount)
 
+        # is_fig_here = 0
         for figure in figures:
             if isinstance(figure, figure_class):
                 y = figure.get_as_y(image_size)
+                # is_fig_here = 1
 
         x_list.append(np.array(im))
         y_list.append(y)
+        # y_is_fig_here_list.append(np.array([1]))
 
     X = np.array(x_list).reshape(pics_amount, image_size, image_size, 1)
     y = np.array(y_list)
+    # y_is_fig_here = np.array(y_is_fig_here_list)
 
-    return X, y, figures
+    return X, y, [1], figures
 
 
 image_size = 100
 
-X_train, y_train, figures_train = generate_data(1000000, image_size, Circle, 1, 5)
-print(np.shape(y_train))
+X_train, y_train, y_is_fig_here, figures_train = generate_data(100, image_size, Circle, 1, 5)
+
 model = get_circle_model(image_size)
 model.fit(X_train, y_train, epochs=3)
-model.save_weights("get_circle.h5")
+# model.save_weights("get_circle.h5")
+# print(np.shape(y_train))
+# print(np.shape(y_is_fig_here))
+
+# model = find_circle_model(image_size)
+# model.fit(X_train, y_is_fig_here, epochs=3)
+# model.save_weights("find_circle.h5")
