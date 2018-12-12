dbef747a40527dc05363dbde78bbe1db450b3b8e
students <voivakin@doriath.yandex.ru>
2018-12-12 12:20:24 -0500

    Refactoring

--- src/validation.py
+++ src/validation.py
@@ -11,6 +11,31 @@ from models.circle import find_circle_model, get_circle_model
 from models.segment import find_segment_model, get_segment_model
 
 
+GET_SEGMENT_MODEL = "GET_SEGMENT_MODEL"
+GET_CIRCLE_MODEL = "GET_CIRCLE_MODEL"
+FIND_SEGMENT_MODEL = "FIND_SEGMENT_MODEL"
+FIND_CIRCLE_MODEL = "FIND_CIRCLE_MODEL"
+
+models = {
+    GET_SEGMENT_MODEL: {
+        "model": get_segment_model,
+        "figure": Segment
+    },
+    GET_CIRCLE_MODEL: {
+        "model": get_circle_model,
+        "figure": Circle
+    },
+    FIND_SEGMENT_MODEL: {
+        "model": find_segment_model,
+        "figure": Segment
+    },
+    FIND_CIRCLE_MODEL: {
+        "model": find_circle_model,
+        "figure": Circle
+    }
+}
+
+
 def generate_data(pics_amount, image_size, figure_class, circles_amount, segments_amount):
     x_list = []
     y_list  = []
@@ -32,17 +57,21 @@ def generate_data(pics_amount, image_size, figure_class, circles_amount, segment
 
 
 image_size = 100
-model = get_circle_model(image_size)
-model.load_weights("get_circle.h5")
+
+model_type = GET_SEGMENT_MODEL
+
+figure_type = models[model_type]["figure"]
+model = models[model_type]["model"](image_size)
+model.load_weights("{}.h5".format(model_type.lower()))
 
 for i in range(10):
-    X_test, y_test, figures_test = generate_data(1, image_size, Circle, 1, 5)
+    X_test, y_test, figures_test = generate_data(1, image_size, figure_type, 1, 1)
 
     res = model.predict(X_test)
     res = res[0]
-    # res = [res[0], res[1], res[3]]
-    segment = Circle.construct_from_y(res, image_size)
-    segment.color = 128
 
-    result_im = generate_pil_image(figures_test + [segment], image_size)
+    figure = figure_type.construct_from_y(res, image_size)
+    figure.color = 128
+
+    result_im = generate_pil_image(figures_test + [figure], image_size)
     save_pil_image(result_im, "pics/test_{}".format(i))
