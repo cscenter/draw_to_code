1396113839a1384695b741c1b522c371a926a645
students <voivakin@almaren.yandex.ru>
2018-12-23 12:56:00 -0500

    Add simple cli

--- src/latex_pic_generator.py
+++ src/latex_pic_generator.py
@@ -27,19 +27,3 @@ def convert_to_latex(figures):
 def convert_to_pic(figures, name, path):
     latex = convert_to_latex(figures)
     save_as_png(name, latex, path)
-
-
-working_dir = "pics"
-
-if not os.path.isdir(working_dir):
-    os.mkdir("pics")
-
-for i in range(1):
-    circle_1 = generate_circle(PLOT_SIDE)
-    circle_2 = generate_circle(PLOT_SIDE)
-    segment_1 = generate_segment(PLOT_SIDE)
-    segment_2 = generate_segment(PLOT_SIDE)
-    triangle = generate_triangle(PLOT_SIDE)
-    figures = [circle_1, circle_2, segment_1, segment_2, *triangle]
-    convert_to_pic(figures, str(i), os.path.join(os.getcwd(), working_dir))
-
--- src/test_model.py
+++ src/test_model.py
@@ -1,18 +1,37 @@
+import argparse
+
 from model import Model
 from pic_generator import generate_random_pic, save_pil_image, load_image, image_to_square
+from latex_pic_generator import convert_to_latex
 
 
-IMAGE_SIZE = 100
-model = Model(IMAGE_SIZE)
-im, figures = generate_random_pic(IMAGE_SIZE, 0, 1)
-im = image_to_square(load_image("test_2.png"), IMAGE_SIZE)
+def arg_parse():
+    parser = argparse.ArgumentParser(description='Convert draw to code.')
+    parser.add_argument('--image', '-im', '-i',
+                        type=str, help='Image', required=True)
+    parser.add_argument('--image_size', '-s',
+                        type=int, help='Image size', default=100)
 
-print('solve')
-circles, segments, images = model.solve(im)
+    args = parser.parse_args()
+    return args
 
-print(circles)
-print(segments)
-print(images)
 
-for i, im in enumerate(images):
-    save_pil_image(im, "pics/test_{}".format(i))
+def main():
+    args = arg_parse()
+    image_size = args.image_size
+    image = args.image
+    im = image_to_square(load_image(image), image_size)
+    model = Model(image_size)
+
+    print('solve')
+    circles, segments, images = model.solve(im)
+
+    for i, im in enumerate(images):
+        save_pil_image(im, "pics/test_{}".format(i))
+
+    latex = convert_to_latex([*circles, *segments])
+    print(latex)
+
+
+if __name__ == '__main__':
+    main()
